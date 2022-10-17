9501e321f9426031b1088e361d1deb7583f1e7f3
KUNJIRA2000 <kun.jira44@hotmail.com>
2022-10-18 02:37:58 +1100

    Initial website commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Recipe from './components/Recipe'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Carbonara from './components/Carbonara'
+import Ramen from './components/Ramen'
+
 
 function App() {
   return (
@@ -11,8 +14,10 @@ function App() {
         <Router>
             <Navbar/>
             <Routes>
-              <Route exact path = "/Home" element = {<Home/>} />
+              <Route exact path = "/" element = {<Home/>} />
               <Route exact path = "/Recipe" element = {<Recipe/>} />
+              <Route exact path = "/Carbonara" element = {<Carbonara/>} />
+              <Route exact path = "/Ramen" element = {<Ramen/>} />
             </Routes>
         </Router>
     </div>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import RecipeContainer from "./RecipeContainer";
+import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import Carbonara from '../components/Carbonara'
+import Ramen from '../components/Ramen'
+
  
 function Recipe() {
 
@@ -8,15 +12,20 @@ function Recipe() {
           <table className="recipe-table">
               <tr>
                   <th className="recipe-column">
+
                     <RecipeContainer
+
+                        linkPath = "/Carbonara"
                         imgPath="https://myfoodbook.com.au/sites/default/files/styles/schema_img/public/recipe_photo/perfect_italiano_fettucine_carbonara.jpg"
                         name="Carbonara"
                         cookingTime="40 minutes"
                         difficulty="Easy"
                     />
+
                   </th>
                   <th className="recipe-column">
                       <RecipeContainer
+                          linkPath = "/Ramen"
                           imgPath="https://cdn.concreteplayground.com/content/uploads/2022/05/ramen-unsplash-1920x1440.jpg"
                           name="Ramen"
                           cookingTime="40 minutes"
