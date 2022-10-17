389617de9f7e1a9f8479a81c1ab1b70f1ff2ecc8
adriantan <adrian.tan@lexicondigital.com.au>
2022-10-17 23:21:42 +1100

    home screen

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import React from 'react'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Recipe from './components/Recipe'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Navbar/>
-            <Routes>
-              <Route exact path = "/Home" element = {<Home/>} />
-              <Route exact path = "/Recipe" element = {<Recipe/>} />
-            </Routes>
-        </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/Home" element={<Home/>}/>
+                    <Route exact path="/Recipe" element={<Recipe/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
