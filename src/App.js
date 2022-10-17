61f9a44125cfe617ea2256a7c129564af18be7c6
adriantan <adrian.tan@lexicondigital.com.au>
2022-10-17 23:44:01 +1100

    add title in recipe page

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
                     <Route exact path="/Recipe" element={<Recipe/>}/>
                 </Routes>
             </Router>
+            <div>
+                <header className ="web-name">FOOD NATION</header>
+            </div>
         </div>
     );
 }
